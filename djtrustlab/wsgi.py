c72ca15a8e821940452bfd6f5af5ca3a6ba7ee96
Dang Vu <dang.vu-nguyen-hai@s2017.tu-chemnitz.de>
2019-09-19 11:52:21 +0200

    add djtrustlab ci-cd

--- djtrustlab/wsgi.py
+++ djtrustlab/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for contenttrustlab project.
+WSGI config for djtrustlab project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'contenttrustlab.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djtrustlab.settings')
 
 application = get_wsgi_application()
