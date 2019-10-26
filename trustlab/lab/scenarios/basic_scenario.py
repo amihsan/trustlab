ac7c168c2cf3df11d156b6d98bf13025e57da124
Valentin Siegert <valentin.siegert@informatik.tu-chemnitz.de>
2019-10-26 20:21:13 +0200

    updated description of basic scenario

--- trustlab/lab/scenarios/basic_scenario.py
+++ trustlab/lab/scenarios/basic_scenario.py
@@ -1,11 +1,11 @@
 # basic scenario for testing
 
 NAME = "Basic Scenario"
+DESCRIPTION = "This is a basic scenario with 3 Agents."
 
 
 AGENTS = ['A', 'B', 'C']
 SCHEDULE = {'A': 3}
-DESCRIPTION = "No one described this scenario so far."
 
 
 
