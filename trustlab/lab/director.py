5a7028fd81f0b233d42fb9363413c97674bd0fff
Valentin Siegert <valentin.siegert@informatik.tu-chemnitz.de>
2020-03-12 15:35:58 +0100

    refactoring of director

--- trustlab/lab/director.py
+++ trustlab/lab/director.py
@@ -10,6 +10,7 @@ class Director:
     def run_scenario(self, scenario):
         thread_server = []
         threads_client = []
+        # creating servers
         for n in range(len(scenario.agents)):
             thread_server.append(n)
             thread_server[n] = AgentServer(n, 2000 + int(n), scenario)
@@ -25,40 +26,18 @@ class Director:
                                          str(history_value) + '\n', 'UTF-8'))
                 director_file.close()
 
-        # with open(s, "r") as fp:
         for observation in scenario.observations:
-            observation_elements = observation.split(',')
-            if str(observation_elements[1]) == 'A':
-                port = 2000
-            if str(observation_elements[1]) == 'B':
-                port = 2001
-            if str(observation_elements[1]) == 'C':
-                port = 2002
-            if str(observation_elements[1]) == 'D':
-                port = 2003
-            if str(observation_elements[1]) == 'E':
-                port = 2004
-            if str(observation_elements[1]) == 'F':
-                port = 2005
-            if str(observation_elements[1]) == 'G':
-                port = 2006
-            if str(observation_elements[1]) == 'H':
-                port = 2007
-            client_thread = AgentClient(str(observation_elements[0]), self.HOST, port, observation)
+            source, target, author, topic, message = observation.split(",", 4)
+            port = 2000 + scenario.agents.index(target)
+            client_thread = AgentClient(source, self.HOST, port, observation)
             threads_client.append(client_thread)
             client_thread.start()
             file_path = Logging.LOG_PATH / "director_log.txt"
             director_file = open(file_path.absolute(), "ab+")
-            director_file.write(
-                bytes(get_current_time() + ' Node: ', 'UTF-8') + bytes(observation_elements[1], 'UTF-8') +
-                bytes(', connection from:', 'UTF-8') +
-                bytes(observation_elements[0], 'UTF-8') + bytes(', author:', 'UTF-8') +
-                bytes(str(observation_elements[2]), 'UTF-8') + bytes(', tag:', 'UTF-8') +
-                bytes(str(observation_elements[3]), 'UTF-8') +
-                bytes(',', 'UTF-8') + bytes(str(scenario.instant_feedback[observation_elements[3]]), 'UTF-8') +
-                bytes(' |message:', 'UTF-8') + bytes(str(observation_elements[4]), 'UTF-8') + bytes('\n', 'UTF-8'))
+            write_string = get_current_time() + ", '" + source + "' will send to '" + target + "' from author: '" + author + "' with topic: " + topic + "," + " the message: " + message + '\n'
+            director_file.write(bytes(write_string, 'UTF-8'))
             director_file.close()
-            time.sleep(0.1)
+            time.sleep(1)
         for thread in threads_client:
             thread.join()
         for server in thread_server:
