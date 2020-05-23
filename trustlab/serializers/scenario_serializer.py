1a406736463ce5c81b2d952cfc813ba4addc6d57
Valentin Siegert <valentin.siegert@informatik.tu-chemnitz.de>
2020-05-23 14:17:32 +0200

    moved Scenario model to trustlab_host

--- trustlab/serializers/scenario_serializer.py
+++ trustlab/serializers/scenario_serializer.py
@@ -17,21 +17,21 @@ class ScenarioSerializer(serializers.Serializer):
 
     def create(self, validated_data):
         return Scenario(validated_data.get('name'), validated_data.get('agents'), validated_data.get('observations'),
+                        validated_data.get('history'), validated_data.get('trust_thresholds'),
+                        validated_data.get('weights'), validated_data.get('metrics_per_agent'),
                         validated_data.get('authorities'), validated_data.get('topics'),
-                        validated_data.get('trust_thresholds'), validated_data.get('weights'),
-                        validated_data.get('metrics_per_agent'), validated_data.get('history'),
                         validated_data.get('description', ""))
 
     def update(self, instance, validated_data):
         instance.name = validated_data.get('name', instance.name)
         instance.agents = validated_data.get('agents', instance.agents)
         instance.observations = validated_data.get('observations', instance.observations)
-        instance.authorities = validated_data.get('authorities', instance.authority)
-        instance.topics = validated_data.get('topics', instance.instant_feedback)
-        instance.trust_thresholds = validated_data.get('trust_thresholds', instance.trust_threshold)
-        instance.weights = validated_data.get('weights', instance.weights)
-        instance.metrics_per_agent = validated_data.get('metrics_per_agent', instance.trust_behavior_1)
         instance.history = validated_data.get('history', instance.history)
+        instance.trust_thresholds = validated_data.get('trust_thresholds', instance.trust_thresholds)
+        instance.weights = validated_data.get('weights', instance.weights)
+        instance.metrics_per_agent = validated_data.get('metrics_per_agent', instance.metrics_per_agent)
+        instance.authorities = validated_data.get('authorities', instance.authorities)
+        instance.topics = validated_data.get('topics', instance.topics)
         instance.description = validated_data.get('description', instance.description)
         return instance
 
