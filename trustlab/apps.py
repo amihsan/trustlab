d2d6b4e38e61118156c157dfaee18f8f3c780846
Shovra <shovra.das@informatik.tu-chemnitz.de>
2020-09-18 01:32:20 +0200

    #23 outdated supervisor registrations detected and handled

--- trustlab/apps.py
+++ trustlab/apps.py
@@ -3,3 +3,9 @@ from django.apps import AppConfig
 
 class TrustlabConfig(AppConfig):
     name = 'trustlab'
+
+    # TODO: Handle corner cases i.e. restricting ready method running multiple times. (See AppConfig doc)
+    def ready(self):
+        Supervisor = self.get_model('Supervisor')
+        Supervisor.objects.filter(agents_in_use=0).delete()
+        print('Outdated supervisors handled')
