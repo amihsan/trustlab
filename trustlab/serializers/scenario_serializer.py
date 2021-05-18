f417f406eaafdd1b6a0f5f5f312789af2a34ff0f
Valentin Siegert <valentin.siegert@informatik.tu-chemnitz.de>
2021-05-18 14:33:31 +0200

    fixed scenario_serializer for new scenario file structure

--- trustlab/serializers/scenario_serializer.py
+++ trustlab/serializers/scenario_serializer.py
@@ -7,8 +7,6 @@ class ScenarioSerializer(serializers.Serializer):
     name = serializers.CharField()
     agents = StringListField()
     observations = serializers.ListField()
-    authorities = serializers.DictField()
-    topics = serializers.DictField()
     trust_thresholds = serializers.DictField()
     weights = serializers.DictField()
     metrics_per_agent = serializers.DictField()
@@ -19,7 +17,6 @@ class ScenarioSerializer(serializers.Serializer):
         return Scenario(validated_data.get('name'), validated_data.get('agents'), validated_data.get('observations'),
                         validated_data.get('history'), validated_data.get('trust_thresholds'),
                         validated_data.get('weights'), validated_data.get('metrics_per_agent'),
-                        validated_data.get('authorities'), validated_data.get('topics'),
                         validated_data.get('description', ""))
 
     def update(self, instance, validated_data):
@@ -30,8 +27,6 @@ class ScenarioSerializer(serializers.Serializer):
         instance.trust_thresholds = validated_data.get('trust_thresholds', instance.trust_thresholds)
         instance.weights = validated_data.get('weights', instance.weights)
         instance.metrics_per_agent = validated_data.get('metrics_per_agent', instance.metrics_per_agent)
-        instance.authorities = validated_data.get('authorities', instance.authorities)
-        instance.topics = validated_data.get('topics', instance.topics)
         instance.description = validated_data.get('description', instance.description)
         return instance
 
